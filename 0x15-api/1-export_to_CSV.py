c41a6f070d17d5206a88647fa9be9a67d69c95f1
daniel <2057@holbertonschool.com>
2021-02-01 21:04:41 -0500

    one

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """Export data in the CSV format"""
-import csv
-import requests
+from csv import writer, QUOTE_ALL
+from csv import QUOTE_ALL
+from requests import get
 from sys import argv
 
 
 if __name__ == '__main__':
-    users = requests.get('https://jsonplaceholder.typicode.com/users',
-                         params={"id": argv[1]}).json()
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos',
-                         params={"userId": argv[1]}).json()
-    for u_name in users:
-        USERNAME = u_name.get('name')
-        USER_ID = u_name.get('id')
+    users = get(
+        'https://jsonplaceholder.typicode.com/users/{:s}'
+        .format(str(argv[1]))).json()
+    todos = get(
+        'http://jsonplaceholder.typicode.com/todos?userId={:s}'
+        .format(str(argv[1]))).json()
     with open('{:s}.csv'.format(argv[1]), mode='w') as f:
-        csv = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
-        for tasks in todos:
-            TASK_STATUS = tasks.get("completed")
-            TASK_TITLE = tasks.get("title")
-            csv.writerow([USER_ID, USERNAME, TASK_STATUS, TASK_TITLE])
+        csv = writer(f, delimiter=',', quotechar='"',
+                          quoting=QUOTE_ALL)
+        for task in todos:
+            csv.writerow([users['id'], users['username'],
+                              task['completed'], task['title']])
