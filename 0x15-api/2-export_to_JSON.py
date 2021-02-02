02b614967616d306e727454fdffd0d9f3069b9a0
daniel <2057@holbertonschool.com>
2021-02-01 22:25:56 -0500

    three

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""  using this REST API, for a given employee ID,
-returns information about his/her TODO list progress"""
+"""export data in the JSON format"""
 import json
 import requests
 from sys import argv
@@ -9,12 +8,11 @@ from sys import argv
 if __name__ == '__main__':
     user_id = argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + 'users/{}'.format(user_id)).json()
-    username = user.get("username")
+    users = requests.get(url + 'users/{}'.format(user_id)).json()
     todos = requests.get(url + 'todos', params={'userId': user_id}).json()
-
+    user_name = users.get("username")
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump({user_id: [{
                 "task": i.get("title"),
                 "completed": i.get("completed"),
-                "username": username} for i in todos]}, jsonfile)
+                "username": user_name} for i in todos]}, jsonfile)
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" script to export data in the JSON format."""
+"""export data in the JSON format"""
 import json
 import requests
 
 
 if __name__ == '__main__':
-    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + 'users').json()
+
     with open("todo_all_employees.json", "w") as jsonfile:
-        for user in users:
-            for task in requests.get(
-                    'https://jsonplaceholder.typicode.com/todos?userId={}'
-                    .format(str(user.get('id')))).json():
-                json.dump({
-                    user.get("id"): [{
-                        "task": task.get("title"),
-                        "completed": task.get("completed"),
-                        "username": user.get("username")
-                    }]}, jsonfile)
+        json.dump({
+            user.get("id"): [{
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": user.get("username")
+            } for task in requests.get(
+                url + "todos?userId=" + str(user.get('id'))).json()]
+            for user in users}, jsonfile)
