93cd91f96ab2ccfd978c017bb50c1fc1c919a563
daniel <2057@holbertonschool.com>
2021-02-04 15:27:16 -0500

    two

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""queries the Reddit API and returns a list containing the titles"""
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """queries the Reddit API and returns a list containing the titles"""
+    response = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={'User-agent': 'your bot 0.1'}, params={'after': after})
+
+    if not response:
+        return None
+    else:
+        rr = response.json().get('data').get('children')
+        for json_dict in rr:
+            hot_list.append(json_dict.get('data').get('title'))
+        if response.json().get('data').get('after') is not None:
+            after = response.json().get('data').get('after')
+            return recurse(subreddit, hot_list, after)
+        else:
+            return hot_list
